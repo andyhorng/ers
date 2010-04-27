46375af6ad8cc3f8b58e76229d758eef125e10d2
Andy Horng <andyhorng168@gmail.com>
2010-04-27 23:05:37 +0800

    調整版面跟 post index 的排序

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ActiveRecord::Base
-  named_scope :post_type, lambda {|type| {:conditions => {:post_type => type} } } 
+  named_scope :post_type, lambda {|type| {:conditions => {:post_type => type}, :order => 'created_at DESC' } } 
 end
